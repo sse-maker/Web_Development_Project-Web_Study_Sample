4eb6f37d13aef936692926884e5ea2e03bc0bdc5
sse-maker <sekwang@postech.ac.kr>
2021-08-11 16:37:08 +0900

    third commit

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,13 @@
-/*
-click(): 해당 요소를 마우스 포인터로 눌렀다 떼었을 때 이벤트 발생
-addClass(): 해당 요소에 클래스 추가
-attr(): 해당 요소에 속성값을 가져옴
-addClass(): 해당 요소에 클래스를 추가
-removeClass(): 해당 요소의 클래스를 지움
-prevAll(): 해당 요소 이전의 모든 요소 선택
-nextAll(): 해당 요소 다음의 모든 요소 선택
-*/
 $(document).ready(function(){
+    /*
+    click(): 해당 요소를 마우스 포인터로 눌렀다 떼었을 때 이벤트 발생
+    addClass(): 해당 요소에 클래스 추가
+    attr(): 해당 요소에 속성값을 가져옴
+    addClass(): 해당 요소에 클래스를 추가
+    removeClass(): 해당 요소의 클래스를 지움
+    prevAll(): 해당 요소 이전의 모든 요소 선택
+    nextAll(): 해당 요소 다음의 모든 요소 선택
+    */
     $("#container").addClass("start");
 	$("nav li").click(function(){
         $("#container").css("max-width", "100%");
@@ -25,5 +25,28 @@ $(document).ready(function(){
         $("nav li").removeClass("on");
         $(".content").removeClass("prev this next");
         $("#container").css("max-width", "1200px");
+    });
+    /*
+    .eq(index): eq 메서드는 요소에서 하나를 선택함, 그러므로 요소의 집합에서 추출할 수 있도록 index가 제공되어야 함 (이때 index는 정수만 가능, 스크립트에서의 숫자는 0으로 시작되기 때문에 첫 번째 태그는 0번째로 표기)
+    .insertAfter(""): 조건에 일치되는 요소 뒤에 해당 요소를 삽입 (A.insertAfter(B)는 B 뒤에 A를 추가하는 것을 뜻함)
+    */
+    $(".roll_left").click(function(){
+        $(".book_roll li").eq(0).insertAfter(".book_roll li:last-child");
+    });
+    $(".roll_right").click(function(){
+        $(".book_roll li").eq(-1).insertBefore(".book_roll li:first-child");
+    });
+    $(".book_roll li").click(function(){
+        var _this =$(this);
+        var liurl =_this.data("url");
+        $(".notebook").html();
+        $.ajax({
+            type : 'post',
+            url : liurl,
+            datatype : 'html',
+            success : function(data) {
+                $(".notebook").html(data);
+            }
+        });
     });
 });
